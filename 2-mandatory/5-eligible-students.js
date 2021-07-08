ab6426c4910efe64b1d6901a263407fdb8ba7e55
Texx45 <tcalderbank@hotmail.com>
2021-07-08 12:01:09 +0100

    passed 5-eligible students

--- 2-mandatory/5-eligible-students.js
+++ 2-mandatory/5-eligible-students.js
@@ -7,7 +7,23 @@
   - Returns an array containing only the names of the who have attended AT LEAST 8 classes
 */
 
-function getEligibleStudents() {}
+function getEligibleStudents(array) {
+  const eligibleStudents = [];
+  const isEligible = array.filter((student) => student[1] >= 8);
+  for (student of isEligible) {
+    eligibleStudents.push(student[0]);
+  }
+  return eligibleStudents;
+}
+
+// function getEligibleStudents(array) {
+//   const eligibleStudents = [];
+//   const isEligible = array.filter((student) => student[1] >= 8);
+//   for (student of isEligible) {
+//     eligibleStudents.push(student[0]);
+//   }
+//   return eligibleStudents;
+// }
 
 /* ======= TESTS - DO NOT MODIFY ===== */
 
